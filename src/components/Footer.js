1d03153a6b38e74f1f087bede8f07416cec1de3d
kishorek0344 <156921551+kishorek0344@users.noreply.github.com>
2024-08-17 12:30:49 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
-        <p className='text-center font-bold' title="E-commerce Website"><h2>
+        <p className='text-center font-bold' title="E-Commerce Website"><h2>
 <font face="Arial" color="#FF5733">G</font>
 <font face="Courier New" color="#28B463">E</font>
 <font face="Verdana" color="#2980B9">N</font>
@@ -20,7 +20,7 @@ const Footer = () => {
 <font face="Trebuchet MS" color="#F1C40F">E</font>
 <font face="Helvetica" color="#E67E22">S</font>
 </h2>
-E-Commerce Website</p></p>
+E-Commerce Website</p>
       </div>
     </footer>
   )
